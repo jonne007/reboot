63e5e8b6ef63d95b1047cd6777056a09d83a97b5
Jonas Nordsten <jonas@openresult.se>
2021-12-12 20:42:29 +0100

    delete

--- reboot/src/Server.scala
+++ reboot/src/Server.scala
@@ -3,12 +3,12 @@ import backend.Bicycle
 object Server extends cask.MainRoutes {
 
   val bb = new InMemoryBicycleBackend
-  
+
   @cask.get("/cycles")
   def list() = {
     val lista = bb.list()
     val cycles = upickle.default.writeJs(lista)
-    ujson.Obj("cycles" -> cycles )
+    ujson.Obj("cycles" -> cycles)
   }
 
   @cask.postJson("/cycles")
@@ -27,38 +27,48 @@ object Server extends cask.MainRoutes {
   }
 
   @cask.get("/cycles/:bid/searchByBrand")
-  def searchByBrand(bid: String) = { 
+  def searchByBrand(bid: String) = {
     val lista = bb.list()
-    val listByBrand = lista.filter(b => b.brand
-    .toLowerCase
-    .contains(bid.toLowerCase))
+    val listByBrand = lista.filter(b =>
+      b.brand.toLowerCase
+        .contains(bid.toLowerCase)
+    )
     val searchedlist = upickle.default.writeJs(listByBrand)
 
     ujson.Obj("brands" -> searchedlist)
   }
 
   @cask.get("/cycles/searchByMaxPrice")
-  def searchByMaxPrice(p: Double) = { 
+  def searchByMaxPrice(p: Double) = {
     val lista = bb.list()
-    val listByPrice = lista.filter(b => b.price <= p )
+    val listByPrice = lista.filter(b => b.price <= p)
     val maxPriceList = upickle.default.writeJs(listByPrice)
 
     ujson.Obj("price" -> maxPriceList)
   }
 
   @cask.get("/cycles/:bid/searchByPricenBrand")
-  def searchByPricenBrand(bid: String, p: Double) = { 
+  def searchByPricenBrand(bid: String, p: Double) = {
     val lista = bb.list()
-    val listaByBrand = lista.filter(b => b.brand
-    .toLowerCase
-    .contains(bid.toLowerCase))
-    val byBrandnPrice = listaByBrand.filter(b => b.price <= p )
+    val listaByBrand = lista.filter(b =>
+      b.brand.toLowerCase
+        .contains(bid.toLowerCase)
+    )
+    val byBrandnPrice = listaByBrand.filter(b => b.price <= p)
     val byBrandnPriceList = upickle.default.writeJs(byBrandnPrice)
 
     ujson.Obj("brandnprice" -> byBrandnPriceList)
   }
 
+  @cask.postJson("/cycles/")
+  def delete(brand: ujson.Value, price: ujson.Value, stock: ujson.Value) = {
+    val b = Bicycle(brand.str, price.num, stock.num.toInt)
+    val marke = b.brand
+    val remove = bb.delete(marke)
+    val removed = upickle.default.writeJs(remove)
 
+    ujson.Obj("removed" -> removed)
+  }
 
   initialize()
 
